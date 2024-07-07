bb625a8508659ff6a0b0d936d0d22b77a7717a6b
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-07-07 07:22:02 -0300

    changes in keep alive ping call

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 import app from "./app.js";
 
 app.listen(process.env.PORT, () => {
+    //executar função a cada 3 minutos +-
+
+    setInterval(async function keepalaiveping() {
+        await fetch("https://casamentobeig.onrender.com/keepaliveping", {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                console.log(response)
+            }).catch(error => {
+                console.log(error);
+            });
+    }, 60000);
+
     console.log("Servidor ouvindo")
 })
